0202d8aa9551052d2d39c165ed870ccb242c4ad1
geogrey12muiruri <greggambugua@gmail.com>
2025-01-30 22:46:45 +0300

    major changes

--- api/routes/schedule.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const scheduleController = require('../controllers/schedule.controller');
-
-router.post('/schedule', scheduleController.addSchedule);
-router.put('/schedule', scheduleController.updateSlot);
-router.get('/schedule/:doctorId', scheduleController.getSchedules);
-
-module.exports = router;
