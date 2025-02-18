91de5364ab835cddffa87573a4f40ed3d9281686
geogrey12muiruri <greggambugua@gmail.com>
2025-02-18 14:27:04 +0300

    use minimal notification context

--- utils/sendPushNotification.ts
+++ utils/sendPushNotification.ts
@@ -12,7 +12,11 @@ export const sendPushNotification = async (expoPushToken: string, title: string,
       body: message,
     };
 
-    await axios.post('https://project03-rj91.onrender.com/send-notification', messagePayload);
+    await axios.post('https://project03-rj91.onrender.com/send-notification', messagePayload, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   } catch (error) {
     console.error('Error sending push notification:', error);
   }
